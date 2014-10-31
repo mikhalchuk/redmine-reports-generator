b0b3f9f05be1fcb677e1bc44ff648368f2619587
Sergii Mykhalchuk <sergii.mykhalchuk@gmail.com>
2014-10-31 12:28:01 +0200

    add generate command. v1

--- src/Application.php
+++ src/Application.php
@@ -6,4 +6,44 @@ use Symfony\Component\Console\Application as ConsoleApplication;
 
 class Application extends ConsoleApplication
 {
+    const OUTPUT_FOLDER = '/monthly_reports/';
+    const CONFIG_PATH = '/config/config.php';
+
+    private $config;
+
+    public function getConfig()
+    {
+        if (empty($this->config)) {
+            $this->config = require_once(dirname(dirname(__FILE__)) . static::CONFIG_PATH);
+        }
+        return $this->config;
+    }
+
+    public function getDateRange()
+    {
+        return [
+            'from' => date($this->getConfig()['date']['from']),
+            'to' => date($this->getConfig()['date']['to']),
+        ];
+    }
+
+    public function getUsers()
+    {
+        return $this->getConfig()['users'];
+    }
+
+    public function getIssuesUrl()
+    {
+        return $this->getConfig()['redmine']['host'];
+    }
+
+    public function getTemplatePath()
+    {
+        return dirname(dirname(__FILE__)) . '/config/template.xls';
+    }
+
+    public function getOutputPath()
+    {
+        return dirname(dirname(__FILE__)) . static::OUTPUT_FOLDER;
+    }
 }
